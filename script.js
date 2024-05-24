c7f6de758315b1f54ef4616d73db5274f8f95509
Pinoy Builders <jlerry005@gmail.com>
2024-05-24 15:18:48 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const currentDateParagraph = document.getElementById('current-date');
+const dateOptionsSelectElement = document.getElementById('date-options');
+
+const date = new Date();
+const day = date.getDate()
+
+const month = date.getMonth() + 1;
+const year = date.getFullYear()
+const hours = date.getHours()
+const minutes = date.getMinutes()
+
+const formattedDate = `${day}-${month}-${year}`
+currentDateParagraph.textContent = formattedDate;
+// const formattedDates = `${year}-${month}-${day}`
+// const formattedDatess = `${month}-${day}-${year} | ${hours}:${minutes}`
+
+// currentDateParagraph.textContent = formattedDatess;
+
+// const exampleSentence = "selur pmaCedoCeerf".split("").reverse().join("-");
+// console.log(exampleSentence)
+
+
+dateOptionsSelectElement.addEventListener('change', () => {
+    switch(dateOptionsSelectElement.value) {
+        case 'yyyy-mm-dd':
+            currentDateParagraph.textContent = formattedDate
+            .split("-")
+            .reverse()
+            .join("-")
+        break;
+        case 'mm-dd-yyyy-h-mm':
+            currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`
+            .split()
+        break;
+        default: 
+            currentDateParagraph.textContent = formattedDate
+        }
+    console.log(formattedDate)
+})
+
+// console.log(dateOptionsSelectElement)
